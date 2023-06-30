eeda90fde47c373cd7907bc087fe7fc6fb54e80b
Anvita <kumaranvita30@gmail.com>
2023-07-01 01:45:03 +0530

    init

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 
+
 class MainApp(App):
     def build(self):
         self.icon = "Calculator.png"
